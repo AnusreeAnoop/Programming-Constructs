c659eb88e04618c371f34d24c012347e301de1b7
Anusree <anoopanusree31@gmail.com>
2020-08-15 13:38:23 +0530

    Comments added

--- functions/degconverstion2.sh
+++ functions/degconverstion2.sh
@@ -1,26 +1,53 @@
 #!/bin/bash -x
+
+#function to perform conversion 
 function deg_conversion() 
 {
 	case $option in
-		1)
+
+		1) #Fahrenheit to Celsius conversion
+
+		#read value in Fahrenheit
 		read -p "Enter value between 32-212 F" value
-		degC=`echo $value - 32 * 0.56 | awk '{ if( $1>32 && $1<212 ) print $(( $1"-"$3"*"$5 ))  }'` 
+
+		#formula C=F-32*5/9
+		degC=`echo $value-32 | awk -F- '{print $1-$2}'` 
+		degC=`echo $degC*0.56 | awk -F* '{print $1*$2}'`
+
+		#display value in Celsius
 		echo $degC ;;
-		2)
+
+		#Celsius to Fahrenheit conversion
+
+
+		2) #Celsius to Fahrenheit conversion
+
+		#read value in Celsius
 		read -p "Enter value between 0-100 C" value
 		echo $value
-		decF=`echo$value * 1.8 + 32 | awk  '{ if( $1>0 && $1<100 ) print $1"*"$3"+"$5  }'` 
+
+		#formula F=C*9/5+32
+		decF=`echo $value*1.8 | awk -F* '{ print $1*$2 }'`
+		decF=`echo $decF+32 | awk -F+ '{print $1+$2}'` 
+
+		#display value in Fahrenheit
 		echo $decF ;;
+
 		*)
 		echo "Invalid option" ;;
 	esac
 }
 
 echo "Conversion Selection"
+
+#print the conversion options available
 echo "1. Fahrenheit to Celsius"
 echo "2. Celsius to Fahrenheit"
+
+#read option
 read -p "Enter option" option
 
+#call the function to perform task
 deg_conversion
 
 
