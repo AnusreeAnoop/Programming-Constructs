26fadda5fd3d4f95eae95a61660e9f8a51c0b078
Anusree <anoopanusree31@gmail.com>
2020-08-18 22:50:19 +0530

    power of 2 until 256 is reached

--- whileloop/powerof2.sh
+++ whileloop/powerof2.sh
@@ -2,10 +2,10 @@
 
 #declare the variable
 count=1
-#limit of variable 
+
+#print the power table of 2 until 256 is reached
 while [[ count -gt 0 ]]
 do
-	#print the power table of 2 until 256 is reached
 	power=$(( 2**$count ))
 	echo "2 power"$count=$power
 	if [[ $power -eq 256 ]]
