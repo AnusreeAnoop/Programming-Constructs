551760454654e2abfa5fc2fc19b1abb8809944eb
Anusree <anoopanusree31@gmail.com>
2020-08-18 23:08:21 +0530

    A gambler starts with Rs100 and places a bet of Re1 untill goes broke or reaches the goal of 200

--- whileloop/gamblermoney.sh
+++ whileloop/gamblermoney.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+
+#variables
 money=100
 count=0
 won=0
+
 while [[ count -ge 0 ]]
 do
 	bet=$((RANDOM%2))
+
 	if [[ $bet -eq 0 ]]
 	then
 		(( money-=1))
@@ -13,9 +17,11 @@ do
 			echo "No more money"
 			break
 		fi
+
 	else
 		(( money+=1 ))
 		(( won+=1 ))
+
 		if [[ $money -eq 200 ]]
 		then
 			echo "Won!!"
@@ -24,5 +30,6 @@ do
 	fi
 	(( count+=1 ))
 done
+
 echo "Number of times won= "$won
 echo "Number of bets made= "$count
