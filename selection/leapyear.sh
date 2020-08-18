5545ce4a3e41936d5b16dcb23b3dd67f57b2c91a
Anusree <anoopanusree31@gmail.com>
2020-08-18 20:24:46 +0530

    checks if year is leap year or not

--- selection/leapyear.sh
+++ selection/leapyear.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 #read year
 read -p "Enter a year" year
@@ -10,14 +10,14 @@ then
 	#year should be divisible by 4
 	if [[ $(($year%4)) -eq 0 ]]
 	then
-		#should not be divisible by 100 
+		#should not be divisible by 100
 		if [[ $(($year%100)) -ne 0 ]]
 		then
 			echo $year "is a leap year"
 		else
 			#if divisible by 100 then year should be divisible by 400
 			if [[ $(($year%400)) -eq 0 ]]
-			then 
+			then
 				echo $year "is a leap year"
 			else
 				echo $year "not a leap year"
