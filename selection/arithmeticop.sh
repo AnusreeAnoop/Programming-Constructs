0edbfd978e857aad2c55f5bc1ad156177b483ccb
Anusree <anoopanusree31@gmail.com>
2020-08-18 20:52:31 +0530

    does arithmetic operations and finds maximum and minimum

--- selection/arithmeticop.sh
+++ selection/arithmeticop.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 
 #read 3 numbers
 read -p "Enter a num" num1
@@ -8,31 +8,38 @@ read -p "Enter a num" num3
 #do arithmetic operation
 operation1=$(( $num1+$num2*$num3 ))
 echo "operation1:" $num1"+"$num2"*"$num3=$operation1
+
 operation2=$(( $num1%$num2+$num3 ))
 echo "operation2:"$num1"%"$num2"+"$num3=$operation2
+
 operation3=$(( $num3+$num1/$num2 ))
 echo "operation3:"$num3"+"$num1"/"$num3=$operation3
+
 operation4=$(( $num1*$num2+$num3 ))
 echo "operation4:"$num1"*"$num2"+"$num3=$operation4
 
 #check which operation gives maximum value
 if [[ $operation1 -gt $operation2 && $operation1 -gt $operation3 ]]
 then
-	echo operation1 "maximum"
+	echo "operation1 maximum"
+
 elif [[ $operation2 -gt $operation1 && $operation2 -gt $operation3 ]]
 then
-	echo operation2 "maximum"
+	echo "operation2 maximum"
+
 else
-	echo operation3 "maximum"
+	echo "operation3 maximum"
 fi
 
 #check which operation gives minimum value
 if [[ $operation1 -lt $operation2 && $operation1 -lt $operation3 ]]
 then
-	echo operation1 "minimum"
+	echo "operation1 minimum"
+
 elif [[ $operation2 -lt $operation1 && $operation2 -lt $operation3 ]]
 then
-	echo operation2 "minimum"
+	echo "operation2 minimum"
+
 else
-	echo operation3 "minimum"
+	echo "operation3 minimum"
 fi
