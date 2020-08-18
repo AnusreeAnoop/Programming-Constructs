699fd7d75544f8fd6295c8c12b6f84374eadc630
Anusree <anoopanusree31@gmail.com>
2020-08-18 23:01:53 +0530

    flip the coin till either head or tail wins 11 times

--- whileloop/coinflip.sh
+++ whileloop/coinflip.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 
 #declare variables
 count=1
@@ -10,27 +10,32 @@ while [[ count -gt 0 ]]
 do
 	#store the flip value
 	flip=$((RANDOM%2))
+
 	if [[ $flip -eq 0 ]]
 	then
 		#increment value when head occures
 		(( HEAD+=1 ))
 		#break if head got repeated 11times
+
 		if [[ $HEAD -eq 11 ]]
 		then
 			echo "HEAD WINS"
 			break
 		fi
+
 	else
 		#increment value when tail occurs
 		(( TAIL+=1 ))
 		#break the loop if tail got repeated 11 times
+
 		if [[ $TAIL -eq 11 ]]
 		then
 			echo "TAIL WINS"
 			break
 		fi
 	fi
-(( count++ ))
+
+	(( count++ ))
 
 done
 
