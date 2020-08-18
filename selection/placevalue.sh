ae0a2c06c75afe895dd04ac84978de5e3ef3fdf4
Anusree <anoopanusree31@gmail.com>
2020-08-18 20:42:33 +0530

    reads num 1,10,100 etc and displays unit,tens,hundred..

--- selection/placevalue.sh
+++ selection/placevalue.sh
@@ -7,24 +7,31 @@ read -p "Enter a number" num
 if [[ $num -eq 1 ]]
 then
         echo "Units"
+
 elif [[ $num -eq 10 ]]
 then
         echo "Tens"
+
 elif [[ $num -eq 100 ]]
 then
         echo "Hundreds"
+
 elif [[ $num -eq 1000 ]]
 then
         echo "Thousands"
+
 elif [[ $num -eq 10000 ]]
 then
         echo "Ten Thousands"
+
 elif [[ $num -eq 100000 ]]
 then
         echo "Lakhs"
+
 elif [[ $num -eq 1000000 ]]
 then
         echo "Ten Lakhs"
+
 else
         echo "Invalid number"
 fi
